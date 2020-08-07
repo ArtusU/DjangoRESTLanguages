571ac471644e2a967278a6d0313e995c1ff8a6d3
ArtusU <artusrock@hotmail.com>
2020-08-07 12:47:19 +0100

    model relationships

--- api_example/languages/serializers.py
+++ api_example/languages/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
-from .models import Language
+from .models import Language, Paradigm, Programer
 
 class LanguageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Language
         fields = ('id', 'url', 'name', 'paradigm')
+
+
+class ParadigmSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Paradigm
+        fields = ('id', 'url', 'name')
+
+
+class ProgramerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Programer
+        fields = ('id', 'url', 'name', 'languages')
 
         
--- api_example/languages/views.py
+++ api_example/languages/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Language
-from .serializers import LanguageSerializer
+from .models import Language, Paradigm, Programer
+from .serializers import LanguageSerializer, ParadigmSerializer, ProgramerSerializer
 
 class LanguageView(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+
+
+class ParadigmView(viewsets.ModelViewSet):
+    queryset = Paradigm.objects.all()
+    serializer_class = ParadigmSerializer
+
+
+class ProgramerView(viewsets.ModelViewSet):
+    queryset = Programer.objects.all()
+    serializer_class = ProgramerSerializer
+
